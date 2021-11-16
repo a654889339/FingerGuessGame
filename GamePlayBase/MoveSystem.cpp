98aed525a9101240521bd77eaee54096a79316e8
ye jiu <a654889339@outlook.com>
2021-11-17 00:37:39 +0800

    ECS Move 8

--- GamePlayBase/MoveSystem.cpp
+++ GamePlayBase/MoveSystem.cpp
@@ -3,30 +3,9 @@
 
 MoveSystem::MoveSystem()
 {
-    m_bEnable        = true;
-    m_pComponentList = NULL;
 }
 
 MoveSystem::~MoveSystem()
 {
 
-}
-
-void MoveSystem::Update(int nUpdateTime)
-{
-    JYLOG_PROCESS_ERROR(m_pComponentList);
-
-    JY_STD_VOID_END
-}
-
-bool MoveSystem::SetComponentList(MoveComponentList* pList)
-{
-    bool bResult = false;
-
-    JYLOG_PROCESS_ERROR(pList);
-    JYLOG_PROCESS_ERROR(!m_pComponentList);
-
-    m_pComponentList = pList;
-
-    JY_STD_BOOL_END
 }
