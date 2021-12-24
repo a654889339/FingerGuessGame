4d9ab6b738531f6b4493f6c698a22f91acd37970
ye jiu <a654889339@outlook.com>
2021-12-25 00:51:11 +0800

    Unreal 1

---
+++ GameEngine/FGGameEngine/Source/FGGameEngine/FGGameEngine.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FGGameEngine.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FGGameEngine, "FGGameEngine" );
---
+++ GameEngine/FGGameEngine/Source/FGGameEngine/FGGameEngineGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FGGameEngineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FGGAMEENGINE_API AFGGameEngineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
