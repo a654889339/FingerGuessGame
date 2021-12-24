7aab4ce803f895c9e63fdb260668906d7fe904d5
ye jiu <a654889339@outlook.com>
2021-12-25 01:04:10 +0800

    Unreal 1

---
+++ GameEngine/Source/GameEngine/GameEngineGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameEngineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEENGINE_API AGameEngineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
